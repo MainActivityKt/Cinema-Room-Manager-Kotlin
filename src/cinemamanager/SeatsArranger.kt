df5bbe3f0d75a1bd9c6a2f7310b55326d2540863
Safi <safiullahkakar2020@outlook.com>
2024-08-20 23:07:14 +0430

    Added stage 1 solution code

---
+++ src/cinemamanager/SeatsArranger.kt
@@ -0,0 +1,30 @@
+package cinemamanager
+
+
+const val TITLE: String = "Cinema"
+const val SPACE: String = " "
+
+fun addRow(rowNumber: Int): String {
+    return buildString {
+        append(rowNumber)
+        append(SPACE)
+        repeat(8) {
+            append("S")
+            append(SPACE)
+        }
+    }
+}
+fun main(args: Array<String>) {
+    println("$TITLE:")
+    val cinema = buildString {
+        repeat(9) {
+            append(if (it > 0) it else SPACE)
+            append(SPACE)
+        }
+        appendLine()
+        for (i in 1..7) {
+            appendLine(addRow(i))
+        }
+    }
+    print(cinema)
+}
